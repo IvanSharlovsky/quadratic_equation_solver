93cf130e85964050687f296620e5bfb8f9136fc9
IvanSharlovsky <ivansharlovsky@gmail.com>
2023-09-25 21:06:41 +0300

    Return function "clear_buffer" to main file

--- clear_buffer.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "clear_buffer.h"
-
-void clear_buffer()
-{
-    while (getchar() != '\n') { ; }
-}
--- quadratic_equation_solver.c
+++ quadratic_equation_solver.c
@@ -3,7 +3,6 @@
 #include <stdbool.h>
 #include <math.h>
 #include <assert.h>
-#include "clear_buffer.h"
 
 //constant for compare format double
 const double ACCURACY = 1e-6;
@@ -17,7 +16,6 @@ enum num_roots
 };
 
 //prototype
-
 int run_unit_test();
 int unit_test(int n_test, double a, double b, double c, int n_roots_ref, double x1_ref, double x2_ref);
 int solve_quad_eq(double a, double b, double c, double* x1, double* x2);
@@ -25,7 +23,10 @@ int solve_line_eq(double b, double c, double* x);
 void num_of_roots(int n_roots, double x1, double x2);
 bool cmp_with_0(double val);
 bool is_equal(double val1, double val2);
+void clear_buffer();
+
 //------------------------------------------------------------------------
+
 int main()
 {
 
@@ -263,4 +264,9 @@ bool cmp_with_0(double val)
 bool is_equal(double val1, double val2)
 {
     return (cmp_with_0(val1 - val2));
+}
+//------------------------------------------------------------------------
+void clear_buffer()
+{
+    while (getchar() != '\n') { ; }
 }
