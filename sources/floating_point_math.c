f10f9c36fa485f538797f063a54a46476db2bd56
IvanSharlovsky <ivansharlovsky@gmail.com>
2023-11-07 21:54:02 +0300

    switch ACCURACY to __DBL_EPSILON__ for more accuracy

--- sources/floating_point_math.c
+++ sources/floating_point_math.c
@@ -4,15 +4,12 @@
 
 #include "../include/floating_point_math.h"
 
-//constant for compare format double
-const double ACCURACY = 1e-6;
-
-bool cmp_with_0(double val) {
-
-    return (fabs(val) < ACCURACY);
+bool cmp_with_0(double val)
+{
+    return (fabs(val) < __DBL_EPSILON__);
 }
 
-bool is_equal(double val1, double val2) {
-
+bool is_equal(double val1, double val2)
+{
     return (cmp_with_0(val1 - val2));
 }
